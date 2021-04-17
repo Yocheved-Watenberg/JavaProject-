df6ddf7b4c7b159e21157170afddf46400863cb3
Yocheved-Watenberg <ywatenbe@g.jct.ac.il>
2021-04-18 00:01:45 +0300

    test pr avi

--- src/primitives/Point3D.java
+++ src/primitives/Point3D.java
@@ -1,17 +1,43 @@
 package primitives;
 
+/**
+ * Class Point3D is the class representing a point in 3D, with 3 coordinates 
+ * 
+ */
+
 public class Point3D {
 	final Coordinate x;
 	final Coordinate y;
 	final Coordinate z;
-	public final static Point3D ZERO = new Point3D(0, 0, 0);
+	
 
+/**
+ * Zero is a static const point3D with the values (0,0,0) 
+ * 
+ */
+	public final static Point3D ZERO = new Point3D(0, 0, 0);
+	
+	
+	 /**
+     * Point3D constructor receiving 3 coordinates values
+     * 
+     * @param x coordinate value x
+     * @param y coordinate value y
+     * @param z coordinate value z
+     */
 	public Point3D(Coordinate x, Coordinate y, Coordinate z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
 
+	 /**
+     * Point3D constructor receiving 3 double values
+     * 
+     * @param x1 value for x
+     * @param x2 value for y
+     * @param x3 value for z
+     */
 	public Point3D(double x1, double x2, double x3) {
 		Coordinate v = new Coordinate(x1);
 		Coordinate w = new Coordinate(x2);
@@ -53,11 +79,21 @@ public class Point3D {
 		return "Point3D [ Coordinates:x=" + x + ", y=" + y + ", z=" + z + "]";
 	}
 
+	/**
+	 * function substract to substract the point from the param
+	 * do : (other - this) 
+	 * @param other 
+	 * @return a new vector after substraction
+	 */
 	public Vector substract(Point3D other) {
 		Point3D myPoint = new Point3D(other.x.coord - x.coord, other.y.coord - y.coord, other.z.coord - z.coord);
 		return new Vector(myPoint);
 	}
 
+	/**
+	 * function add to add the @param other to the point3D
+	 * @return ................
+	 */
 	public Point3D add(Vector other) {
 		return new Point3D(x.coord + other.head.x.coord, y.coord + other.head.y.coord, z.coord + other.head.z.coord);
 	}

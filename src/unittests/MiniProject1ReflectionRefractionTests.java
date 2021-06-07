7813d415020241e22bd19b8dd24a77980cdecb37
Yocheved-Watenberg <ywatenbe@g.jct.ac.il>
2021-06-07 21:36:15 +0300

    qq tests

--- src/unittests/MiniProject1ReflectionRefractionTests.java
+++ src/unittests/MiniProject1ReflectionRefractionTests.java
@@ -31,9 +31,15 @@ public class MiniProject1ReflectionRefractionTests{
 				.setViewPlaneSize(150, 150).setDistance(1000);
 
 		scene.geometries.add( //
-				new Sphere( new Point3D(0, 0, 0),50) 
+				new Sphere( new Point3D(-30, 0, 0),10) 
 						.setEmission(new Color(java.awt.Color.BLUE)) 
-						.setMaterial(new Material().setKd(0.4).setKs(0.3).setnShininess(100).setkT(0.3)));
+						.setMaterial(new Material().setKd(0.4).setKs(0.3).setnShininess(100).setkT(0.8)),
+				new Sphere( new Point3D(0, 0, 0),10) 
+						.setEmission(new Color(java.awt.Color.BLUE)) 
+						.setMaterial(new Material().setKd(0.4).setKs(0.3).setnShininess(100)),
+				new Sphere( new Point3D(30, 0, 0),10) 
+						.setEmission(new Color(java.awt.Color.BLUE)) 
+						.setMaterial(new Material().setKd(0.4).setKs(0.3).setnShininess(100).setkR(0.8)));
 
 		scene.lights.add( //
 				new SpotLight(new Color(1000, 600, 0), new Point3D(-100, -100, 500), new Vector(-1, -1, -2)) //
@@ -53,9 +59,15 @@ public class MiniProject1ReflectionRefractionTests{
 				.setViewPlaneSize(150, 150).setDistance(1000);
 
 		scene.geometries.add( //
-				new Sphere( new Point3D(0, 0, 0),50) 
-						.setEmission(new Color(java.awt.Color.BLUE)) 
-						.setMaterial(new Material().setKd(0.4).setKs(0.3).setnShininess(100).setkT(0.3)));
+				new Sphere( new Point3D(-30, 0, 0),10) 
+				.setEmission(new Color(java.awt.Color.BLUE)) 
+				.setMaterial(new Material().setKd(0.4).setKs(0.3).setnShininess(100).setkT(0.8)),
+		new Sphere( new Point3D(0, 0, 0),10) 
+				.setEmission(new Color(java.awt.Color.BLUE)) 
+				.setMaterial(new Material().setKd(0.4).setKs(0.3).setnShininess(100)),
+		new Sphere( new Point3D(30, 0, 0),10) 
+				.setEmission(new Color(java.awt.Color.BLUE)) 
+				.setMaterial(new Material().setKd(0.4).setKs(0.3).setnShininess(100).setkR(0.8)));
 
 		scene.lights.add( //
 				new SpotLight(new Color(1000, 600, 0), new Point3D(-100, -100, 500), new Vector(-1, -1, -2)) //
@@ -67,6 +79,70 @@ public class MiniProject1ReflectionRefractionTests{
 				.setRayTracerBase(new RayTracerBasic(scene));
 		render.renderImageBeamOfRay();
 		render.writeToImage();
+	}
+	
+	
+	@Test
+	public void AudeSans() {
+		Camera camera = new Camera(new Point3D(0, 0, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
+				.setViewPlaneSize(150, 150).setDistance(500);
+
+		scene.geometries.add( //
+				new Triangle(new Point3D(  -500, 500,-1000),
+						 new Point3D( 500, 500,-1000),
+						 new Point3D(  0,  200, -1000)).setEmission(new Color(0, 0, 255)),
+				 new Triangle( new Point3D(  0, 200, -1000),
+						  new Point3D( 500, 500, -1000),
+						  new Point3D( 0, 0, -1000)).setEmission(new Color(0, 255, 0)),
+				 new Triangle(new Point3D(  0, 200, -1000),
+						  new Point3D( -500,  500,-1000),
+						  new Point3D( 0,0,-1000 )).setEmission(new Color(255, 0, 0)),
+				 new Sphere( new Point3D(0, -250, -1000), 150).setEmission(new Color(0, 100, 100))
+				 .setMaterial(new Material().setnShininess(20).setkT(0.5)) );
+
+		scene.lights.add( //
+				new SpotLight(new Color(255, 100, 100), new Point3D(200, 200, -150), new Vector(-2, -2, -3)) //
+						.setKl( 0.00001).setKq(0.000005).setKc(0.1));
+
+		Render render = new Render() //
+				.setImageWriter(new ImageWriter("audesans", 500, 500)) //
+				.setCamera(camera) //
+				.setRayTracerBase(new RayTracerBasic(scene));
+		render.renderImage();
+		render.writeToImage();
+
+	}
+	
+	
+	@Test
+	public void AudeAvec() {
+		Camera camera = new Camera(new Point3D(0, 0, 1000), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
+				.setViewPlaneSize(150, 150).setDistance(500);
+
+		scene.geometries.add( //
+				new Triangle(new Point3D(  -500, 500,-1000),
+						 new Point3D( 500, 500,-1000),
+						 new Point3D(  0,  200, -1000)).setEmission(new Color(0, 0, 255)),
+				 new Triangle( new Point3D(  0, 200, -1000),
+						  new Point3D( 500, 500, -1000),
+						  new Point3D( 0, 0, -1000)).setEmission(new Color(0, 255, 0)),
+				 new Triangle(new Point3D(  0, 200, -1000),
+						  new Point3D( -500,  500,-1000),
+						  new Point3D( 0,0,-1000 )).setEmission(new Color(255, 0, 0)),
+				 new Sphere( new Point3D(0, -250, -1000), 150).setEmission(new Color(0, 100, 100))
+				 .setMaterial(new Material().setnShininess(20).setkT(0.5)) );
+
+		scene.lights.add( //
+				new SpotLight(new Color(255, 100, 100), new Point3D(200, 200, -150), new Vector(-2, -2, -3)) //
+						.setKl( 0.00001).setKq(0.000005).setKc(0.1));
+
+		Render render = new Render() //
+				.setImageWriter(new ImageWriter("audeavec", 500, 500)) //
+				.setCamera(camera) //
+				.setRayTracerBase(new RayTracerBasic(scene));
+		render.renderImageBeamOfRay();
+		render.writeToImage();
+
 	}
 	
 }
